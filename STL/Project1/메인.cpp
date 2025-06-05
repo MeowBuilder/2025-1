777ceaa211112af29ba7b7eb8809a0eca6862eeb
MeowBuilder <96215662+MeowBuilder@users.noreply.github.com>
2025-06-05 19:04:07 +0900

    update 0605

--- STL/Project1/메인.cpp
+++ STL/Project1/메인.cpp
@@ -1,17 +1,13 @@
 //-----------------------------------------------------------
-// 2025.1 학기		 STL 6월 2일 월요일				(13주 1일)
+// 2025.1 학기		 STL 6월 5일 목요일				(13주 2일)
 // 6/19 목요일 15주 2일 - 기말시험
 //-----------------------------------------------------------
-// Associative Container
-// - set<key>			: key와 value를 구분하지 않는다.
-// - Map<key, value>	: <아이돌그룹, 멤버>
+// Unordered Associative Container - hash 기반
 //-----------------------------------------------------------
 
 #include <iostream>
-#include <fstream>
-#include <algorithm>
-#include <set>
-#include <array>
+#include "string"
+#include <unordered_set>
 #include "save.h"
 #include "STRING.h"
 
@@ -19,45 +15,21 @@ using namespace std;
 
 extern bool 관찰;
 
+// trade - off				찾기 성능(time)		공간 성능(memory)
+// Sequence					- O(N)				- N
+// Associative				- O(log N)			- N + pointer
+// Unordered Associative	- O(1)				- N + table + pointer
+
 int main(){
+	unordered_set<string> us{ "1","22","333","4444","55555" };
 
-	ifstream Alice{ "이상한 나라의 앨리스.txt" };
-	if (not Alice)
+	for (const string& s : us)
 	{
-		cout << "파일이 없다." << endl;
-		return 18651126;
+		cout << s << endl;
 	}
 
-	multiset<STRING> s{ istream_iterator<STRING>{Alice},{} };
-	cout << "ㄷㅏㄴㅇㅓㅇㅡㅣ ㄱㅐㅅㅜ :" << s.size() << endl;
-
-	// [문제] multiset<STRING> s에 저장된 모든 알파벳의 사용횟수를 다음과 같이
-	// 사용된 횟수기준 내림차순으로 출력하라
-	// 대문자는 소문자로 바꾸자
-	// e - 13618
-	// t - 10726
-	// ...
-	// z - 78
-
-	array<int, 26> a{};
-
-	for (const STRING& word : s)
-	{
-		for (char ch : word)
-		{
-			if (isalpha(ch))
-			{
-				ch = tolower(ch);
-				a[ch - 'a']++;
-			}
-		}
-	}
-
-
-	for (size_t i = 0; i < a.size(); ++i)
-	{
-		cout << static_cast<char>('a' + i) << " - " << a[i] << endl;
-	}
+	cout << std::hash<string>{}("55555") << endl;
+	cout << std::hash<string>{}("4444") << endl;
 
 	//save("메인.cpp");
 }
